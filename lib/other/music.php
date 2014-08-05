ade96819d2a40df949fe6d25b8223b8ec3903301
Johan Ehrendahl <johan.ehrendahl@binarysword.com>
2014-08-05 06:07:03 +0200

    Music Volume
    You can now add volume for the music format, but SoundCloud API and
    YouTube API v3 is still being worked on.

--- lib/other/music.php
+++ lib/other/music.php
@@ -4,7 +4,7 @@
 // To enable this, you need to set the YT Video/MP3/URL, the name and the format.
 // --------------
 // Example Usage:
-// array("http://MyURL.com", "My Awesome Title", "Format"),
+// array("http://MyURL.com", "My Awesome Title", "Format", "Volume"),
 // --------------
 // Playlists is also supported for:
 // SoundCloud - just include the set
@@ -16,13 +16,13 @@
 // SoundCloud | Include the whole SoundCloud URL, sets also work
 // --------------
 $RandMusic = array(
-	//array("http://www.youtube.com/watch?v=H-k_Eg7zXuc", "The Glitch Mob - We Can Make The World Stop", "YouTube"),
-	//array("http://www.youtube.com/embed/videoseries?list=PL0OmcxG5_BQ00O30bjmire70JZFwXhb86", "Music List", "YouTubeList"),
-	//array("http://my-site.com/music/example.mp3", "Example Artist - Example Song", "MP3"),
-	//array("https://soundcloud.com/fantomenk/sets/chipz", "FantomenK - ChIpz (Playlist)", "SoundCloud"),
-	//array("https://soundcloud.com/robothuggles/sets/robot-language", "Meganeko - Robot Language (Playlist)", "SoundCloud"),
-	//array("https://soundcloud.com/floydthewolf/song-of-the-last-man-long", "Floyd! - Song Of The Last Man", "SoundCloud"),
-	//array("https://soundcloud.com/the-void-music/tvdfree-012-rob-gasser-blend", "[TVDFREE-012] - Rob Gasser & Blend - Exhale (The Void Bootleg)", "SoundCloud"),
-	//array("https://soundcloud.com/robgassermusic/rob-gasser-blend-exhale-2", "Rob Gasser & Blend - Exhale (Original Mix)", "SoundCloud"),
+	array("http://www.youtube.com/watch?v=H-k_Eg7zXuc", "The Glitch Mob - We Can Make The World Stop", "YouTube", "15"),
+	//array("http://www.youtube.com/embed/videoseries?list=PL0OmcxG5_BQ00O30bjmire70JZFwXhb86", "Music List", "YouTubeList", "75"),
+	//array("http://my-site.com/music/example.mp3", "Example Artist - Example Song", "MP3", "75"),
+	//array("https://soundcloud.com/fantomenk/sets/chipz", "FantomenK - ChIpz (Playlist)", "SoundCloud", "75"),
+	//array("https://soundcloud.com/robothuggles/sets/robot-language", "Meganeko - Robot Language (Playlist)", "SoundCloud", "75"),
+	//array("https://soundcloud.com/floydthewolf/song-of-the-last-man-long", "Floyd! - Song Of The Last Man", "SoundCloud", "75"),
+	//array("https://soundcloud.com/the-void-music/tvdfree-012-rob-gasser-blend", "[TVDFREE-012] - Rob Gasser & Blend - Exhale (The Void Bootleg)", "SoundCloud", "75"),
+	//array("https://soundcloud.com/robgassermusic/rob-gasser-blend-exhale-2", "Rob Gasser & Blend - Exhale (Original Mix)", "SoundCloud", "75"),
 );
 ?>
--- lib/other/music_init.php
+++ lib/other/music_init.php
@@ -14,11 +14,40 @@ if($RandMusic[$num][0] != "")
 		echo '<span style="position:absolute;top:5px;float:right;right:5px;font-size:22px;text-shadow: 0px 0px 4px rgba(255, 255, 255, 1);"><img src="images/icons/music.png" style="box-shadow: 0px 0px 15px rgba(50, 50, 50, 0);" width="16"> '. $RandMusic[$num][1] . ' <img src="images/icons/music.png" style="box-shadow: 0px 0px 15px rgba(50, 50, 50, 0);" width="16"></span>';
 	}
 	elseif($RandMusic[$num][2] == "MP3") {
-		echo "<embed src='music/player.swf' id='radioplayer' name='radioplayer' quality='medium' allowScriptAccess='always' width='1' height='1' type='application/x-shockwave-flash' FlashVars='file=". $RandMusic[$num][0] ."&volume=75&start=0&duration=0&autostart=true&controlbar=none&dock=false&icons=false'>";
+		echo "<embed src='music/player.swf' id='radioplayer' name='radioplayer' quality='medium' allowScriptAccess='always' width='1' height='1' type='application/x-shockwave-flash' FlashVars='file=". $RandMusic[$num][0] ."&volume=". $RandMusic[$num][3] ."&start=0&duration=0&autostart=true&controlbar=none&dock=false&icons=false'>";
 		echo '<span style="position:absolute;top:5px;float:right;right:5px;font-size:22px;text-shadow: 0px 0px 4px rgba(255, 255, 255, 1);"><img src="images/icons/music.png" style="box-shadow: 0px 0px 15px rgba(50, 50, 50, 0);" width="16"> '. $RandMusic[$num][1] . ' <img src="images/icons/music.png" style="box-shadow: 0px 0px 15px rgba(50, 50, 50, 0);" width="16"></span>';
 	}
 	elseif($RandMusic[$num][2] == "SoundCloud") {
-		echo '<iframe width="0" height="0" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url='.$RandMusic[$num][0].'&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_artwork=true"></iframe>';
-		echo '<span style="position:absolute;top:5px;float:right;right:5px;font-size:22px;text-shadow: 0px 0px 4px rgba(255, 255, 255, 1);"><img src="images/icons/music.png" style="box-shadow: 0px 0px 15px rgba(50, 50, 50, 0);" width="16"> '. $RandMusic[$num][1] . ' <img src="images/icons/music.png" style="box-shadow: 0px 0px 15px rgba(50, 50, 50, 0);" width="16"></span>';
+		echo '
+			<iframe width="0" height="0" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url='.$RandMusic[$num][0].'&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_artwork=true"></iframe>
+			<script src="https://w.soundcloud.com/player/api.js" type="text/javascript"></script>';
+		echo "
+			<script type=\"text/javascript\">
+			  (function(){
+				var widgetIframe = document.getElementById('sc-widget'),
+					widget       = SC.Widget(widgetIframe);
+
+				widget.bind(SC.Widget.Events.READY, function() {
+				  widget.bind(SC.Widget.Events.PLAY, function() {
+					// get information about currently playing sound
+					widget.getCurrentSound(function(currentSound) {
+					  console.log('sound ' + currentSound.get('') + 'began to play');
+					});
+				  });
+				  // get current level of volume
+				  widget.getVolume(function(volume) {
+					console.log('current volume value is ' + volume);
+				  });
+				  // set new volume level
+				  widget.setVolume(". $RandMusic[$num][3] .");
+				  // get the value of the current position
+				});
+
+			  }());
+			</script>
+			";
+		echo'
+			<span style="position:absolute;top:5px;float:right;right:5px;font-size:22px;text-shadow: 0px 0px 4px rgba(255, 255, 255, 1);"><img src="images/icons/music.png" style="box-shadow: 0px 0px 15px rgba(50, 50, 50, 0);" width="16"> '. $RandMusic[$num][1] . ' <img src="images/icons/music.png" style="box-shadow: 0px 0px 15px rgba(50, 50, 50, 0);" width="16"></span>
+		';
 	}
 }
