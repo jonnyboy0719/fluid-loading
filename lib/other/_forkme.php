188b2673a7f62b0eae77fe9732c789ba13405977
Johan Ehrendahl <johan.ehrendahl@binarysword.com>
2014-08-08 20:42:03 +0200

    Added GitHub ribbon
    Can easily be removed from lib/other/

---
+++ lib/other/_forkme.php
@@ -0,0 +1,3 @@
+<a href="https://github.com/jonnyboy0719/fluid-loading">
+	<img style="position: absolute; bottom: 0; left: 0; border: 0;box-shadow: unset;z-index: 60;" src="img/forkme.png" alt="Fork me on GitHub" data-canonical-src="img/forkme.png">
+</a>
