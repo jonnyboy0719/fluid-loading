63b3ce3ca15db26ab2b06afb6f46c57cee670941
GreenBlack <green@ttt-fun.com>
2014-09-14 13:00:15 -0700

    Update 03.php

--- Templates/03.php
+++ Templates/03.php
@@ -63,8 +63,8 @@ require_once "lib/config.php";
 				echo "
 			<img src=" . $row["avatarfull"] . " width='100'>
 			<ul>
-				<li><img src='images/icons/user.png' width='16'> <span>" . $row["personaname"] . "</span></li>
-				<li><img src='images/icons/key.png' width='16'> <span>STEAMID: <span class='steamid'>" . $steamid . "</span></span></li>
+				<li><img src='images/icons/user.png' width='16'> <span>" . htmlspecialchars($row["personaname"]) . "</span></li>
+				<li><img src='images/icons/key.png' width='16'> <span>STEAMID: <span class='steamid'>" . htmlspecialchars($steamid) . "</span></span></li>
 				<li><img src='images/flags/{$ShowFlag}.png' width='16'> <span>Connecting from <span class='country'>" . $flags[$ShowFlag] . "</span></span></li>
 			</ul>";
 			} else {
